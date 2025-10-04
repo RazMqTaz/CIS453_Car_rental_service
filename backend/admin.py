ac6dce40289dca02bcc5db364a21daca20467c23
Erazem Mattick <erazemmattick@Erazems-MacBook-Pro.local>
2025-10-03 23:13:37 -0400

    fixed small things

--- backend/admin.py
+++ backend/admin.py
@@ -1,8 +1,11 @@
 from .reservation import Reservation
 from .user import User
 
+
 class Admin(User):
-    def __init__(self, user_id: int, name: str, email: str, license_number: str) -> None:
+    def __init__(
+        self, user_id: int, name: str, email: str, license_number: str
+    ) -> None:
         super().__init__(user_id, name, email, license_number, role="admin")
 
     # Define class for viewing/cancel reservations from database, implementation depends on what database we use
